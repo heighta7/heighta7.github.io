b52b9f88c6bc7df623c35ad422b45117db0de65e
heighta7 <99057410+heighta7@users.noreply.github.com>
2023-07-03 21:06:02 +1000

    Update graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -1,30 +1,87 @@
 const Graphics = {};
-Graphics.list = ['r1', 'r2', 'm1', 'f1_md', 'f1_lg', 'f1_xl'];
+Graphics.list = [
+  "r1",
+  "r2",
+  "m1",
+  "f1_md",
+  "f1_lg",
+  "f1_xl",
+  "fs1_md",
+  "fs1_lg",
+  "fs1_xl",
+];
 Graphics.getSrc = (graphic) => {
-    if (graphic === 'r1') { return 'img/r1.png'; }
-    if (graphic === 'r2') { return 'img/r2.png'; }
-    if (graphic === 'm1') { return 'img/m1.png'; }
-    if (graphic === 'f1_md') { return 'img/f1-md.png'; }
-    if (graphic === 'f1_lg') { return 'img/f1-lg.png'; }
-    if (graphic === 'f1_xl') { return 'img/f1-xl.png'; }
+  if (graphic === "r1") {
+    return "img/r1.png";
+  }
+  if (graphic === "r2") {
+    return "img/r2.png";
+  }
+  if (graphic === "m1") {
+    return "img/m1.png";
+  }
+  if (graphic === "f1_md") {
+    return "img/f1-md.png";
+  }
+  if (graphic === "f1_lg") {
+    return "img/f1-lg.png";
+  }
+  if (graphic === "f1_xl") {
+    return "img/f1-xl.png";
+  }
+  if (graphic === "fs1_md") {
+    return "img/fs1-md.png";
+  }
+  if (graphic === "fs1_lg") {
+    return "img/fs1-lg.png";
+  }
+  if (graphic === "fs1_xl") {
+    return "img/fs1-xl.png";
+  }
 };
 Graphics.get = (graphic) => {
-    return pixi.loader.resources[graphic].texture;
+  return pixi.loader.resources[graphic].texture;
 };
 Graphics.forPerson = (person) => {
-    return Graphics.get(person.graphic);
+  return Graphics.get(person.graphic);
 };
 Graphics.addToLoader = (loader) => {
-    Graphics.list.forEach(x => loader.add(x, Graphics.getSrc(x)));
+  Graphics.list.forEach((x) => loader.add(x, Graphics.getSrc(x)));
 };
 Graphics.getHeelsPositions = (graphic) => {
-    if (graphic === 'f1_md') { return [{ x1: 0.28, x2: 0.48 }, { x1: 0.52, x2: 0.72 }]; }
-    if (graphic === 'f1_lg') { return [{ x1: 0.28, x2: 0.48 }, { x1: 0.52, x2: 0.72 }]; }
-    if (graphic === 'f1_xl') { return [{ x1: 0.28, x2: 0.48 }, { x1: 0.52, x2: 0.72 }]; }
-    if (graphic === 'm1') { return [{ x1: 0.25, x2: 0.45 }, { x1: 0.55, x2: 0.75 }]; }
+  if (graphic === "f1_md") {
+    return [
+      { x1: 0.28, x2: 0.48 },
+      { x1: 0.52, x2: 0.72 },
+    ];
+  }
+  if (graphic === "f1_lg") {
+    return [
+      { x1: 0.28, x2: 0.48 },
+      { x1: 0.52, x2: 0.72 },
+    ];
+  }
+  if (graphic === "f1_xl") {
+    return [
+      { x1: 0.28, x2: 0.48 },
+      { x1: 0.52, x2: 0.72 },
+    ];
+  }
+  if (graphic === "m1") {
+    return [
+      { x1: 0.25, x2: 0.45 },
+      { x1: 0.55, x2: 0.75 },
+    ];
+  }
+  if (graphic === "fs1_md" || graphic === "fs1_lg" || graphic === "fs1_xl") {
+    return [{ x1: 0.03, x2: 0.82 }];
+  }
 
-    return [{ x1: 0.25, x2: 0.45 }, { x1: 0.55, x2: 0.75 }];
+  return [
+    { x1: 0.25, x2: 0.45 },
+    { x1: 0.55, x2: 0.75 },
+  ];
 };
 Graphics.heelsPositionsForPerson = (person) => {
-    return Graphics.getHeelsPositions(person.graphic);
+  return Graphics.getHeelsPositions(person.graphic);
 };
