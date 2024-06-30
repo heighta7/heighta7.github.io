aa4727b2cf128092b12d69bccbfac782477f107d
heighta7 <99057410+heighta7@users.noreply.github.com>
2024-06-30 21:18:30 +1000

    Update edit-height.js

--- js/widget/edit-height.js
+++ js/widget/edit-height.js
@@ -1,42 +1,66 @@
 const EditHeightWidget = {};
-EditHeightWidget.name = 'edit-height-widget';
+EditHeightWidget.name = "edit-height-widget";
 
 $(() => {
-    $('#edit-height-widget .close').on('click', () => { EditHeightWidget.close(); });
-    $('#edit-height-widget .averages').on('click', () => { EditHeightWidget.averages(); });
-    $('#edit-height-widget .height-feet .picker-up').on('click', () => { EditHeightWidget.heightButton(Height.FOOT); });
-    $('#edit-height-widget .height-feet .picker-down').on('click', () => { EditHeightWidget.heightButton(-Height.FOOT); });
-    $('#edit-height-widget .height-inches .picker-up').on('click', () => { EditHeightWidget.heightButton(Height.INCH); });
-    $('#edit-height-widget .height-inches .picker-down').on('click', () => { EditHeightWidget.heightButton(-Height.INCH); });
-    $('#edit-height-widget .height-inch-tenths .picker-up').on('click', () => { EditHeightWidget.heightButton(Height.INCH / 10); });
-    $('#edit-height-widget .height-inch-tenths .picker-down').on('click', () => { EditHeightWidget.heightButton(-Height.INCH / 10); });
+  $("#edit-height-widget .close").on("click", () => {
+    EditHeightWidget.close();
+  });
+  $("#edit-height-widget .advanced").on("click", () => {
+    EditHeightWidget.advanced();
+  });
+  $("#edit-height-widget .height-feet .picker-up").on("click", () => {
+    EditHeightWidget.heightButton(Height.FOOT);
+  });
+  $("#edit-height-widget .height-feet .picker-down").on("click", () => {
+    EditHeightWidget.heightButton(-Height.FOOT);
+  });
+  $("#edit-height-widget .height-inches .picker-up").on("click", () => {
+    EditHeightWidget.heightButton(Height.INCH);
+  });
+  $("#edit-height-widget .height-inches .picker-down").on("click", () => {
+    EditHeightWidget.heightButton(-Height.INCH);
+  });
+  $("#edit-height-widget .height-inch-tenths .picker-up").on("click", () => {
+    EditHeightWidget.heightButton(Height.INCH / 10);
+  });
+  $("#edit-height-widget .height-inch-tenths .picker-down").on("click", () => {
+    EditHeightWidget.heightButton(-Height.INCH / 10);
+  });
 });
 
 EditHeightWidget.init = () => {
-    EditHeightWidget.updateData();
+  EditHeightWidget.updateData();
 };
 EditHeightWidget.close = () => {
-    Widget.changeWidget(PersonWidget);
+  Widget.changeWidget(PersonWidget);
 };
-EditHeightWidget.averages = () => {
-    Widget.changeWidget(AverageHeightsWidget);
+EditHeightWidget.advanced = () => {
+  Widget.changeWidget(HeightMenuWidget);
 };
 EditHeightWidget.updateData = () => {
-    let person = People.getSelected();
-    let imperial = Height.asImperial(person.height);
-    $('#edit-height-widget .height-feet p').text(`${imperial.feet.toFixed()}'`);
-    $('#edit-height-widget .height-inches p').text(`${imperial.inches.toFixed()}`);
-    $('#edit-height-widget .height-inch-tenths p').text(`.${imperial.inchTenths.toFixed()}"`);
-    
-    $('#edit-height-widget .height-metric p').text(Height.displayStringMetric(person.height));
-    $('#edit-height-widget .weight p').text(Weight.displayString(person.calculateWeightKg()));
+  let person = People.getSelected();
+  let imperial = Height.asImperial(person.height);
+  $("#edit-height-widget .height-feet p").text(`${imperial.feet.toFixed()}'`);
+  $("#edit-height-widget .height-inches p").text(
+    `${imperial.inches.toFixed()}`
+  );
+  $("#edit-height-widget .height-inch-tenths p").text(
+    `.${imperial.inchTenths.toFixed()}"`
+  );
+
+  $("#edit-height-widget .height-metric p").text(
+    Height.displayStringMetric(person.height)
+  );
+  $("#edit-height-widget .weight p").text(
+    Weight.displayString(person.calculateWeightKg())
+  );
 };
 EditHeightWidget.heightButton = (cm) => {
-    let person = People.getSelected();
-    let newHeight = person.height + cm;
-    if (newHeight < 0) {
-        newHeight = 0;
-    }
-    person.height = Height.toNearestInchTenth(newHeight);
-    EditHeightWidget.updateData();
+  let person = People.getSelected();
+  let newHeight = person.height + cm;
+  if (newHeight < 0) {
+    newHeight = 0;
+  }
+  person.height = Height.toNearestInchTenth(newHeight);
+  EditHeightWidget.updateData();
 };
